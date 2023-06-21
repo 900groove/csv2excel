4088b487eb7965a81e6aca843ed6a3090e58d29a
hdymacuser <hdymacuser@MC020102.local>
2023-06-21 14:34:10 +0900

    add black

--- src/csv2excel/cli.py
+++ src/csv2excel/cli.py
@@ -3,9 +3,9 @@ from csv2excel import convert
 
 
 def main():
-    parser = argparse.ArgumentParser(description='入力されたcsvファイルをexcelに変換')
-    parser.add_argument('input_file', help='対象のファイル')
-    parser.add_argument('output_file', help='出力ファイル')
+    parser = argparse.ArgumentParser(description="入力されたcsvファイルをexcelに変換")
+    parser.add_argument("input_file", help="対象のファイル")
+    parser.add_argument("output_file", help="出力ファイル")
     args = parser.parse_args()
 
     convert(input_file=args.input_file, output_file=args.output_file)
--- tests/test_csv2excel.py
+++ tests/test_csv2excel.py
@@ -5,12 +5,12 @@ import pandas as pd
 class TextConverter(unittest.TestCase):
     def test_converter(self):
         from csv2excel import convert
+
         input_file = "./tests/fixture.csv"
         output_file = "./tests/output.xlsx"
         convert(input_file=input_file, output_file=output_file)
         input_df = pd.read_csv(input_file)
         output_df = pd.read_excel(output_file)
 
-        # 入力csvと書き出したexcelが一致することを確認 
+        # 入力csvと書き出したexcelが一致することを確認
         self.assertTrue(input_df.equals(output_df))
-
